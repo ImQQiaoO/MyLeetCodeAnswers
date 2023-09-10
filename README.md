723f0fa7a10adaff80d70ca4a7c8cc855483179e
ImQQiaoO <imqqiaoo@gmail.com>
2023-09-10 22:01:48 +0800

    feat: LeetCode112

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 深度优先搜索(DFS):
 - [LeetCode104 | C++](./cpp/LeetCode104/main.cpp)
 - [LeetCode111 | C++](./cpp/LeetCode111/main.cpp)
-- [LeetCode112 | C++](./cpp/LeetCode112/main.cpp)       *正在写！*
+- [LeetCode112 | C++](./cpp/LeetCode112/main.cpp)
+    - 递归
 - [LeetCode1123 | C++](./cpp/LeetCode1123/main.cpp)      ***没弄懂！***
--- cpp/LeetCode112/main.cpp
+++ cpp/LeetCode112/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
-
 
 struct TreeNode {
     int val;
@@ -17,30 +17,57 @@ struct TreeNode {
 
 class Solution {
 public:
-    int minDepth(TreeNode *root) {// dfs
+    bool hasPathSum(TreeNode *root, int targetSum) {
         if (root == nullptr) {
-            return 0;
+            return false;
         }
-        int curr = 0;
-        int min = INT_MAX;
-        dfs(root, curr, min);
-        return min;
-    }
-
-    void dfs(TreeNode *node, int curr, int &min) {
-        if (node == nullptr) {
-            return;
+        if (root->left == nullptr && root->right == nullptr) {
+            return targetSum == root->val;
         }
-        if (node->left == nullptr && node->right == nullptr) {
-            min = std::min(min, curr + 1);
-            return;
-        }
-        curr++;
-        dfs(node->left, curr, min);
-        dfs(node->right, curr, min);
+        // 如果当前节点不是叶子节点，则递归判断其子节点
+        return hasPathSum(root->left, targetSum - root->val) ||
+            hasPathSum(root->right, targetSum - root->val);
     }
 };
 
 int main() {
-    cout << "hi" <<  endl;
+    // root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+    auto *root = new TreeNode(5);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(11);
+    root->left->right = nullptr;
+    root->right->left = new TreeNode(13);
+    root->right->right = new TreeNode(4);
+    root->left->left->left = new TreeNode(7);
+    root->left->left->right = new TreeNode(2);
+    root->right->left->left = nullptr;
+    root->right->left->right = nullptr;
+    root->right->right->left = nullptr;
+    root->right->right->right = new TreeNode(1);
+    Solution solution;
+    int result = solution.hasPathSum(root, 22);
+    cout << result << endl;
+
+    // root = [1,2,3], targetSum = 5
+    auto *root2 = new TreeNode(1);
+    root2->left = new TreeNode(2);
+    root2->right = new TreeNode(3);
+    Solution solution2;
+    int result2 = solution2.hasPathSum(root2, 5);
+    cout << result2 << endl;
+
+    // root = [1,2], targetSum = 1
+    auto *root3 = new TreeNode(1);
+    root3->left = new TreeNode(2);
+    Solution solution3;
+    int result3 = solution3.hasPathSum(root3, 1);
+    cout << result3 << endl;
+
+    auto *root4 = new TreeNode(1);
+    Solution solution4;
+    int result4 = solution4.hasPathSum(root4, 1);
+    cout << result4 << endl;
+
+    return 0;
 }
