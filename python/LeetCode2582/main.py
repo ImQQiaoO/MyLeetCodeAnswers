fdcb44d67d7fc860ae37a3f425184dede4d7c2ae
ImQQiaoO <imqqiaoo@gmail.com>
2023-09-26 11:25:50 +0800

    style: Change python -> LeetCode2582 to Python3

--- python/LeetCode2582/main.py
+++ python/LeetCode2582/main.py
@@ -1,8 +1,7 @@
-class Solution(object):
-    def passThePillow(self, n, time):
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
         t = time % (n - 1)
         return n - t if time // (n - 1) % 2 else t + 1
-
 
 s1 = Solution()
 print(s1.passThePillow(4, 5))
